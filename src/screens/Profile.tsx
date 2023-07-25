58b7e5f19f7145f765eecb60e4fd0ca72b4ff004
Gustavo Oliveira <guga.oli.1357@gmail.com>
2023-07-25 01:42:00 -0300

    Tela de perfil finalizada

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,16 @@
 import { StyleSheet } from "react-native"
-import { Heading, VStack, Text } from 'native-base';
+import { Heading, VStack, HStack, Text, Link } from 'native-base';
 import { SafeAreaView as View } from 'react-native-safe-area-context';
 
 import Ellipse from '../imgs/ellipse2.svg';
 import BigUser from '../imgs/bg-user-solid.svg';
+import SMAdCard from '../imgs/ad-card-sm.svg';
+import SMLocateDot from '../imgs/location-dot-sm.svg';
+import SMWhatsapp from '../imgs/wpp-sm.svg';
+import SMEnvelope from '../imgs/envelope-sm.svg';
+import SMBCake from '../imgs/bcake-sm.svg';
+import SMHandshake from '../imgs/handshake-sm.svg';
+import SMEditPen from '../imgs/edit-pen-sm.svg';
 
 export default function Profile(){
   return (
@@ -13,15 +20,52 @@ export default function Profile(){
           <Heading pt={2} mt={"-3.5rem"} fontSize="3xl" color='#FFF' textAlign='center'>Gustavo Oliveira Souza</Heading>
         </VStack>
 
-        <VStack backgroundColor="#777" p={6} w={180} h={180} borderRadius={'full'} justifyItems="center" mt={50}>
-          <VStack ml={3.5}>
+        <VStack backgroundColor="#777" w={180} h={180} borderRadius={'full'} justifyItems="center"alignItems="center" mt={6}>
+          <VStack mt={6}>
             <BigUser />
           </VStack>
+        </VStack>
 
-          <VStack mt={60} justifyItems='center'>
-            <Text textAlign='center'>22 anos</Text>
-            <Text textAlign='center'>Muriaé - MG</Text>
-          </VStack>
+        <VStack mt={6} width="80%" >
+          <Heading textAlign='center'>22 anos</Heading>
+          <Heading textAlign='center' fontSize='xl' pb={4} mb={4} borderBottomWidth={3} borderBottomColor="#000">Muriaé - MG</Heading>
+          
+          <HStack my={2} alignItems="center">
+            <SMAdCard />
+            <Text ml={2} fontSize='lg' color="#000" bold fontSize='lg' color="#000" bold>123.456.789-10</Text>
+          </HStack>
+
+          <HStack my={2} alignItems="center">
+            <SMLocateDot />
+            <Text ml={2} fontSize='lg' color="#000" bold>Lorem ipsum dolor sit amet</Text>
+          </HStack>
+
+          <HStack my={2} alignItems="center">
+            <SMWhatsapp />
+            <Text ml={2} fontSize='lg' color="#000" bold>(32)98709-9174</Text>
+          </HStack>
+
+          <HStack my={2} alignItems="center">
+            <SMEnvelope />
+            <Text ml={2} fontSize='lg' color="#000" bold>guga.oli.1357@gmail.com</Text>
+          </HStack>
+
+          <HStack my={2} alignItems="center">
+            <SMBCake />
+            <Text ml={2} fontSize='lg' color="#000" bold>06 de Dezembro de 2000</Text>
+          </HStack>
+
+          <HStack my={2} alignItems="center">
+            <SMHandshake />
+            <Text ml={2} fontSize='lg' color="#000" bold>0 solicitações feitas</Text>
+          </HStack>
+        
+          <HStack justifyContent="center" mt={5}>
+            <Link>
+              <SMEditPen />
+              <Text ml={2} fontSize='lg' color="#00ADB5" bold>Editar Perfil</Text>
+            </Link>
+          </HStack>
         </VStack>
     </View>
 
