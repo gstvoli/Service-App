fc2c48b792a9893d403c901a9fbe6f6e3a944833
Gustavo Oliveira <guga.oli.1357@gmail.com>
2023-07-30 02:47:28 -0300

    Arquivo de rotas criado e configurado

--- App.tsx
+++ App.tsx
@@ -1,18 +1,10 @@
 import { NativeBaseProvider, Heading, VStack } from 'native-base';
-
-import Home from './src/screens/Home';
-import SignIn from './src/screens/SignIn';
-import SignInTerms from './src/screens/SignInTerms';
-import SignInCard from './src/screens/SignInCard'
-import SignInFinish from './src/screens/SignInFinish';
-import Login from './src/screens/Login';
-import Services from './src/screens/Services';
-import Profile from './src/screens/Profile';
+import Routes from './routes';
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <Home />
+      <Routes />
     </NativeBaseProvider>
   );
 }
--- routes.js
+++ routes.js
@@ -1,3 +1,36 @@
-const express = require('express');
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const routes = express.Router();
+import Home from './src/screens/Home';
+import SignIn from './src/screens/SignIn';
+import SignInTerms from './src/screens/SignInTerms';
+import SignInCard from './src/screens/SignInCard';
+import SignInFinish from './src/screens/SignInFinish';
+import Login from './src/screens/Login';
+import Services from './src/screens/Services';
+import Profile from './src/screens/Profile';
+
+const Stack = createNativeStackNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="SignInTerms" component={SignInTerms} />
+        <Stack.Screen name="SignInCard" component={SignInCard} />
+        <Stack.Screen name="SignInFinish" component={SignInFinish} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Services" component={Services} />
+        <Stack.Screen name="Profile" component={Profile} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+// const express = require('express');
+
+// const routes = express.Router();
