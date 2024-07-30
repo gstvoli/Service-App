94d3379f899de7bafd13b4dcf67bbbbcca43d8d5
Gustavo Oliveira <guga.oli.1357@gmail.com>
2024-07-30 17:06:53 -0300

    Restarting the repo with new structure

--- App.tsx
+++ App.tsx
@@ -1,18 +1,25 @@
-import { NativeBaseProvider } from 'native-base';
-import { Routes } from './src/routes';
-import { useFonts, Inter_100Thin, Inter_200ExtraLight, Inter_300Light, Inter_400Regular, Inter_500Medium, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold, Inter_900Black } from '@expo-google-fonts/inter';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { PaperProvider, useTheme } from 'react-native-paper';
 
-
-import 'react-native-gesture-handler';
-import { Loading } from './src/components/Loading';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({Inter_100Thin, Inter_200ExtraLight, Inter_300Light, Inter_400Regular, Inter_500Medium, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold, Inter_900Black})
+  const theme = useTheme();
   return (
-    <NativeBaseProvider>
-      {fontsLoaded ? <Routes /> : <Loading />}
-      {/* <Service /> */}
-    </NativeBaseProvider>
+    <PaperProvider>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- src/@types/svg.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "*.svg" {
-  import React, { ReactText } from 'react';
-  import { SvgProps } from 'react-native-svg';
-  const content: ReactText.FC<SvgProps>;
-  export default content;
-}
