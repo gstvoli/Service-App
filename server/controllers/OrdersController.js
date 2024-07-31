0b537f4ce4cfc24c404463d2eadc6c236fdf7dbe
Gustavo Oliveira <guga.oli.1357@gmail.com>
2024-07-30 22:03:29 -0300

    Configuration of server-side files

---
+++ server/controllers/OrdersController.js
@@ -0,0 +1,65 @@
+const connection = require('../database/connection');
+const bcrypt = require('bcrypt');
+
+module.exports = {
+  async create(request, response) {
+    const {
+      data_abertura,
+      servico,
+      observacao,
+      status,
+      valor,
+      acrescimo,
+      desconto,
+      rua_servico,
+      bairro_servico,
+      numcasa_servico,
+      complemento_servico,
+      cidade_servico,
+      uf_servico,
+      id_cliente,
+      id_colaborador
+    } = request.body;
+    console.log(request.body);
+
+    const newOrder = await connection('pedido').insert({
+      data_abertura: data_abertura,
+      servico: servico,
+      observacao: observacao,
+      status: status,
+      valor: valor,
+      acrescimo: acrescimo,
+      desconto: desconto,
+      rua_servico: rua_servico,
+      bairro_servico: bairro_servico,
+      numcasa_servico: numcasa_servico,
+      complemento_servico: complemento_servico,
+      cidade_servico: cidade_servico,
+      uf_servico: uf_servico,
+      id_cliente: id_cliente,
+      id_colaborador: id_colaborador
+    });
+
+    return response.json(newOrder);
+  },
+
+  async list(request, response) {
+    const { userId } = request.params;
+    const order = await connection('pedido')
+      .where('pedido.id_cliente', userId)
+      .select('pedido.*', 'colaborador.nome as colaborador')
+      .leftJoin('colaborador', 'colaborador.id', 'pedido.id_colaborador');
+
+    return response.json(order);
+  },
+
+  async index(request, response) {
+    const { id } = request.params;
+    const order = await connection('pedido')
+      .where('pedido.id', id)
+      .select('pedido.*', 'colaborador.nome as colaborador')
+      .leftJoin('colaborador', 'colaborador.id', 'pedido.id_colaborador');
+
+    return response.json(order);
+  }
+};
---
+++ server/database/connection.js
@@ -0,0 +1,11 @@
+const knex = require('knex').knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: "37226614123gG#'",
+    database: 'serviceapp'
+  }
+});
+
+module.exports = knex;
